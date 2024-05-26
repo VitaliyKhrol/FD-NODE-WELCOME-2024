89f678fa43a018fb44dc4c9fd945a66029436fa6
Vitaliy K <vitaliyx@gmail.com>
2024-05-26 23:37:39 +0300

    Task resolve

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fc = require ('fs').promises;
 
-const promice = fc.readFile('./text.txt', {encoding:'utf8'})
-.then (promiseValue =>{
-            const fileText = promiseValue + 'We can read !!!!!!!!!!!!!';
-            fc.writeFile('./text2.txt', fileText)
-})
+const textPromise = fc.readFile('text.txt', 'utf-8');
 
+const userDataPromise = fc.readFile('./userData.json', 'utf-8');
 
+Promise.all([textPromise, userDataPromise])
+    .then(([textData, userData]) => {
+        const obj = JSON.parse(userData);
+        const fullName = `Hello, ${obj.firstName} ${obj.lastName} ${textData}`;
+        return fc.writeFile('text1.txt', fullName);
+    })
+    .then(() => {
+        console.log('Содержимое успешно записано в файл text1.txt');
+    })
+    .catch((error) => {
+        console.error('Произошла ошибка:', error);
+    });
